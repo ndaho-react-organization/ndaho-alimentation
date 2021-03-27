fbae5fc0ce171b97cb3b73df688774c81a7eacbb
ndayiho <ndayiho5@gmail.com>
2021-03-27 19:07:58 +0100

    Mise en place d'une architecture de la gestion des etats --> REDUX!!

--- src/components/Erreur404.js
+++ src/components/Erreur404.js
@@ -1,7 +1,7 @@
 const Erreur404 = props => {
     return (
         <div className="text-center">
-            <img src={process.env.PUBLIC_URL + `/assets/error404.png`} alt="image 404" width="300" />
+            <img src={process.env.PUBLIC_URL + `/assets/error404.png`} alt="page 404" width="300" />
             <p>La page que vous cherchez n'existe pas </p>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from "./lib/store"
+import { addToCart } from './lib/actions';
+
+//
+console.log(store.getState());
+
+const unsubscribe = () => store.unsubscribe(() => console.log(store.getState()));
+store.dispatch(addToCart({ name: "Citron", prix: "1€" }, 2))
+console.log(store.getState());
+store.dispatch(addToCart({ name: "Kiwi", prix: "1€" }, 5))
+console.log(store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
